82af643f4ecf41bf614827ea10cfd573f5f04446
karunialeo <karunialeo@gmail.com>
2021-06-06 00:09:59 +0700

    after modify gitignore

---
+++ vendor/composer/installed.php
@@ -0,0 +1,76 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'v4.1.2',
+    'version' => '4.1.2.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'codeigniter4/appstarter',
+  ),
+  'versions' => 
+  array (
+    'codeigniter4/appstarter' => 
+    array (
+      'pretty_version' => 'v4.1.2',
+      'version' => '4.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'codeigniter4/framework' => 
+    array (
+      'pretty_version' => 'v4.1.2',
+      'version' => '4.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0bf61820417a3e4a6e7f6c725c5cccf285a1b7c4',
+    ),
+    'kint-php/kint' => 
+    array (
+      'pretty_version' => '3.3',
+      'version' => '3.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '335ac1bcaf04d87df70d8aa51e8887ba2c6d203b',
+    ),
+    'laminas/laminas-escaper' => 
+    array (
+      'pretty_version' => '2.7.0',
+      'version' => '2.7.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5e04bc5ae5990b17159d79d331055e2c645e5cc5',
+    ),
+    'laminas/laminas-zendframework-bridge' => 
+    array (
+      'pretty_version' => '1.2.0',
+      'version' => '1.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6cccbddfcfc742eb02158d6137ca5687d92cee32',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'zendframework/zend-escaper' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^2.6.1',
+      ),
+    ),
+  ),
+);
